9120b4a36f22ea45c96065647bc1c41abb961a91
wzpziyi1 <1204473090@qq.com>
2015-10-12 14:44:39 +0800

    Initial Commit

---
+++ ZYMusicPlayer/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ZYMusicPlayer
+//
+//  Created by 王志盼 on 15/10/12.
+//  Copyright © 2015年 王志盼. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
